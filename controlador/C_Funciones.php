04fe2551f177084fb1aa90153f49b015f03ac1cf
flavio157 <82402824+flavio157@users.noreply.github.com>
2021-05-04 17:16:51 -0500

    se elimino codigo repetido y se cambio el return por un array en funcion fechas

--- controlador/C_Funciones.php
+++ controlador/C_Funciones.php
@@ -3,34 +3,30 @@ date_default_timezone_set('America/Lima');
 
 
     function fechas($diasprimeraquincena,$diassegundaquincena){
-       
-        $fecha_actual = date("d-m-Y");
         $fecha = getdate();
-        $dia = date("d-m-Y",strtotime($fecha_actual."- 1 days"));
+        $dia = date("d-m-Y",strtotime(date("d-m-Y")."- 1 days"));
         $mes = $fecha['mon'];
         $ano = $fecha['year'];
         $m = intval($mes) - intval(1); 
             if($fecha['mday']>= $diasprimeraquincena[0] && $fecha['mday'] <= '26'){
-                if($mes <= '9'){
-                    $mes = '0'.$mes;
-                }
-                $fechainicial = '12'.'-'. $mes .'-'.$ano;
-                $fechafinal =  $dia;
+                if($mes <= '9'){$mes = '0'.$mes; }
+                    $fechainicial = '12'.'-'. $mes .'-'.$ano;
+                    $fechafinal =  $dia;
             }
             if($fecha['mday'] >= $diassegundaquincena[0] && $fecha['mday'] <= '11'){
-                    if($m <= '9'){
-                        $m = '0'.$m;
-                    }
+                    if($m <= '9'){ $m = '0'.$m;}
                     $fechainicial = '27'.'-'. $m .'-'.$ano;
                     $fechafinal =  $dia;
             }
-            return $fechainicial . " " .$fechafinal; 
+
+            return array($fechainicial,$fechafinal);
+     
     }
 
 
 
     function f_Cuotas($verificarCuotas,$cuotas,$diasprimeraquincena,$diassegundaquincena){
-    $cuotas = $cuotas == '' ? '0' : $cuotas;
+        $cuotas = $cuotas == '' ? '0' : $cuotas;
         $hoy = getdate();
         if($hoy['mday'] >= $diasprimeraquincena[0] && round($cuotas,2)  > round($verificarCuotas,2)   && $hoy['mday'] <=$diasprimeraquincena[1]){
 
