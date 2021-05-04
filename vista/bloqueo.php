4c683d1123a0153e828cb7329277623d94594a7b
flavio157 <82402824+flavio157@users.noreply.github.com>
2021-05-04 14:16:03 -0500

    pagina donde sale el mensaje de bloque al usuario

--- vista/bloqueo.php
+++ vista/bloqueo.php
@@ -1,12 +1,4 @@
-<?php
-    require_once("../controlador/C_Cuotas.php");
-    $m_cuota = new C_Controlar_Cuotas();
-    $cantidad = $_GET['cantidad'];
-    $cuota = $_GET['cuota'];
-?>
 <html>
-
-
     <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
@@ -19,8 +11,7 @@
     </head>
     <body>
         <div>
-            <center> <h2>Su Usuario ha sido bloquedo su promedio de ventas hasta hoy es <?php echo $cantidad ?>
-            Su promedio a alcanzar es de <?php echo $cuota ?> </h2></center>
+            <center> <h2>Su Usuario ha sido bloquedo</h2></center>
         </div>
     </body>
 </html>
