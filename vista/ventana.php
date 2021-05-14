8b420e62b0ebfec4e4db17f7f575cf16861081c2
flavio157 <82402824+flavio157@users.noreply.github.com>
2021-05-14 08:07:19 -0500

    se movieron los modales de ingresar el producto y mostrar los pedidos

--- vista/ventana.php
+++ vista/ventana.php
@@ -1,4 +1,5 @@
 <?php
+
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
@@ -9,6 +10,7 @@ if(!isset($_SESSION['zona'])){
 } 
 date_default_timezone_set('America/Lima');
 $fcha = date("Y-m-d",strtotime(date("Y-m-d")."+ 1 days"));
+
 ?>
 
 <html>
@@ -33,6 +35,9 @@ $fcha = date("Y-m-d",strtotime(date("Y-m-d")."+ 1 days"));
     <body>
     <div class="main">
         <form class="row g-3"  id="frmpedidos">
+                <input type="text" id="vroficina" style="display: none;" value="<?php echo $_SESSION['oficina']?>"/>
+                <input type="text" id="vrzona" style="display: none;" value="<?php echo $_SESSION['zona']?>"/>
+                <input type="text" id="vrcodpersonal" style="display: none;" value="<?php echo $_SESSION['cod_personal']?>"/>
                 <div id="mensajesgenerales">
                                    
                 </div>
@@ -196,145 +201,8 @@ $fcha = date("Y-m-d",strtotime(date("Y-m-d")."+ 1 days"));
                 </div>
         </form>
 
-                   <!-- Modal1 para ingresar el pedido de producto -->
-                   <div class="modal fade " id="ModalProducto" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-x2">
-                        <div class="modal-content">
-                        <div class="modal-header">
-                            <h6 class="modal-title" id="exampleModalLabel">DESCRIPCION DEL PEDIDO</h6>
-                            <button type="button"  id="closemodal" class="btn-close" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <form id="frmagregarProducto">
-                                  <div id="mensaje">
-                                   
-                                   </div>
-                                <div class="col">
-                                    <label for="formfpago" class="form-label">PRODUCTO</label>
-                                    <input type="text" style="text-transform:uppercase" class="form-control" name="nombreproducto" id="nombreproducto" autocomplete="off">
-                                </div>
-                                <div id="sugerencias"></div>
-                                
-                               
-                                <div class="row g-2">
-                                    <div class="col">
-                                        <label  for="formentrega" class="form-label">COD. PRODUCTO</label>
-                                        <input type="text" class="form-control"  id="cod_producto"  disabled=true>
-                                    </div>
-                                </div>
-                                <div class="row g-2">
-                                    <div class="col">
-                                        <label for="formcantidad" class="form-label">CANTIDAD</label>
-                                        <input type="number" class="form-control"  name="G_cantidad" id="G_cantidad" autocomplete="off">
-                                    </div>
-                                    <div class="col">
-                                        <label for="formfpago" class="form-label">PRECIO</label>
-                                        <input type="number" class="form-control" name="precioproducto" id="precioproducto" disabled=true>
-                                    </div>
-                                </div>
-
-                                <div class="row g-2">
-                                    <div class="col">
-                                        <label for="formpromocion" class="form-label">PROMOCION</label>
-                                        <input type="number" class="form-control" name="G_promocion" autocomplete="off" id="G_promocion">
-                                    </div>
-                                    <div class="col">
-                                        <label for="formfpago" class="form-label">TOTAL</label>
-                                        <input type="number" class="form-control" id="G_total" name="G_total" disabled=true>
-                                    </div>
-                                </div>
-                                <div class="card-body d-flex justify-content-between align-items-center">
-                                        Lista de Productos
-                                        <a id="agregarProducto" class="btn btn-primary btn-sm">+</a>
-                                </div>
-
-                                <div class="table-responsive tablafrmpedidos" id="tablaproductos">
-                                    <table class="table  table-striped  table-sm productosMomento" id="productosMomento">
-                                        <caption>Lista de Productos</caption>
-                                        <thead>
-                                            <tr>
-                                            <th scope="col" style="display: none;">COD_PRODUCTO</th>
-                                            <th scope="col">PRODUCTO</th>
-                                            <th scope="col">CANTIDAD</th>
-                                            <th scope="col">PRECIO</th>
-                                            <th scope="col">PROMO</th>
-                                            <th scope="col" style="display: none;">TOTAL</th>
-                                            <th scope="col">ACCIONES</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody  id="tablaModal">
-                                                                
-                                        </tbody>
-                                    </table>
-                </div>
-                            </form>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary" id="agregar">Agregar</button>
-                        </div>
-                        </div>
-                    </div>
-                </div>
-
-                <!--modal para mostrar los pedidos ya registrados el dia actual responsive -->
-                <div class="modal fade" id="ModalMostrarPedidos" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLongTitle">Pedidos Registrados</h5>
-                            </div>
-                            <div class="modal-body">
-
-                                <div class="accordion accordion-flush" id="acordionresponsive">
-                                    
-                                </div>
-
-
-                                <div class="table-responsive " id="tbedidosRegistrados">
-                                    <table class="table table-hover ">
-                                        <thead>
-                                            <tr>
-                                            <th scope="col">#</th>
-                                            <th scope="col">First</th>
-                                            <th scope="col">Last</th>
-                                            <th scope="col">Handle</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                            <th scope="row">1</th>
-                                            <td>Mark</td>
-                                            <td>Otto</td>
-                                            <td>@mdo</td>
-                                            </tr>
-                                            <tr>
-                                            <th scope="row">2</th>
-                                            <td>Jacob</td>
-                                            <td>Thornton</td>
-                                            <td>@fat</td>
-                                            </tr>
-                                            <tr>
-                                            <th scope="row">3</th>
-                                            <td colspan="2">Larry the Bird</td>
-                                            <td>@twitter</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-
-
-
-
-
-                            </div>
-
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" id="cerrarmodal" data-dismiss="modal">Cerrar</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+        <?php include 'Modal_IngresarProducto.php';?>
+        <?php include 'Modal_ListarPedidos.php';?>
 
         </div>
     </body>
