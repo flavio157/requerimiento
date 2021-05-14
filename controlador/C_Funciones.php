26308c4eb55950c34cd9ce38566f704e2b7516db
flavio157 <82402824+flavio157@users.noreply.github.com>
2021-05-14 17:15:11 -0500

    se esta modificando la funcion fechas para no bloquear al usuario si se esta registrando un dia  antes de la verificacion de la cuota

--- controlador/C_Funciones.php
+++ controlador/C_Funciones.php
@@ -2,14 +2,39 @@
 date_default_timezone_set('America/Lima');
 
 
-    function fechas($diasprimeraquincena,$diassegundaquincena){
+    function fechas($diasprimeraquincena,$diassegundaquincena,$fec_ingreso){
         $fecha = getdate();
-        $dia = date("d-m-Y",strtotime(date("d-m-Y")."- 1 days"));
         $mes = $fecha['mon'];
         $ano = $fecha['year'];
+        $fech = explode(" ",$fec_ingreso);
+        $separando = explode("-",$fech[0]);
+        $dia = date("d-m-Y",strtotime(date("d-m-Y")."- 1 days"));
+
+        if($mes <= '9'){$mes = '0'.$mes; }
         $m = intval($mes) - intval(1); 
-            if($fecha['mday'] >= $diasprimeraquincena[0] && $fecha['mday'] <= '26'){
-                if($mes <= '9'){$mes = '0'.$mes; }
+        
+        $diaingreso1 = $diasprimeraquincena[0] - $separando[2];
+
+        $date1=date_create($diassegundaquincena[0]."-".$mes."-".$ano);
+        $date2=date_create($fec_ingreso);
+        $diaingreso2 = date_diff($date2,$date1);
+
+        if($separando[0] == $ano && $separando[1] == $mes && $diaingreso1 == 1 || $diaingreso1 <= 0 && $separando[2] <= '26') {
+                if( $separando[2] >= ($diasprimeraquincena[0]-1)){
+                   return header("Location: http://localhost:8080/requerimiento/vista/ventana.php");
+                }
+
+        }else if($separando[0] == $ano  && ( $mes - $separando[1]) == 0 ||
+                ($mes - $separando[1]) == 1 && $diaingreso2->d == 1 || $diaingreso2->d == 0 ||
+                $separando[2] <= '11')          
+        {
+                        return $separando[1] -1;
+        }
+        
+        
+        
+        
+        else if($fecha['mday'] >= $diasprimeraquincena[0] && $fecha['mday'] <= '26'){
                     $fechainicial = '12'.'-'. $mes .'-'.$ano;
                     $fechafinal =  $dia;
             }
@@ -18,28 +43,45 @@ date_default_timezone_set('America/Lima');
                     $fechainicial = '27'.'-'. $m .'-'.$ano;
                     $fechafinal =  $dia;
             }
-            return array($fechainicial,$fechafinal);
+            if($diaingreso1 < 0){
+               
+            }
+            
+           /* array($fechainicial,$fechafinal);*/
     }
+
+
+
 
 
 
     function f_Cuotas($verificarCuotas,$cuotas,$diasprimeraquincena,$diassegundaquincena){
-        $cuotas = $cuotas == '' ? '0' : $cuotas;
-        $hoy = getdate();
-        if($hoy['mday'] >= $diasprimeraquincena[0] && round($cuotas,2)  > round($verificarCuotas,2)   && $hoy['mday'] <=$diasprimeraquincena[1]){
+        
+       if($cuotas != '0' && $cuotas != null){
+            $hoy = getdate();
+            if($hoy['mday'] >= $diasprimeraquincena[0] && round($cuotas,2)  > round($verificarCuotas,2)   && $hoy['mday'] <=$diasprimeraquincena[1]){
 
-            if( $hoy['mday'] == $diasprimeraquincena[0] || $hoy['mday'] <= $diasprimeraquincena[1] ){
-                return header("Location:http://localhost:8080/requerimiento/vista/bloqueo.php");
+                if( $hoy['mday'] == $diasprimeraquincena[0] || $hoy['mday'] <= $diasprimeraquincena[1] ){
+                    return header("Location:http://localhost:8080/requerimiento/vista/bloqueo.php");
+                }
+            }else if($hoy['mday'] >= $diassegundaquincena[0] &&  round($cuotas,2)  > round($verificarCuotas,2)  && $hoy['mday'] <= $diassegundaquincena[1]){
+            
+                if($hoy['mday'] == $diassegundaquincena[0] || $hoy['mday'] <= $diassegundaquincena[1]){
+                    return header("Location:http://localhost:8080/requerimiento/vista/bloqueo.php");
+                } 
+            }else{
+                return header("Location: http://localhost:8080/requerimiento/vista/ventana.php");
             }
-        }else if($hoy['mday'] >= $diassegundaquincena[0] &&  round($cuotas,2)  > round($verificarCuotas,2)  && $hoy['mday'] <= $diassegundaquincena[1]){
-          
-            if($hoy['mday'] == $diassegundaquincena[0] || $hoy['mday'] <= $diassegundaquincena[1]){
-                return header("Location:http://localhost:8080/requerimiento/vista/bloqueo.php");
-            } 
-        }else{
-            return header("Location: http://localhost:8080/requerimiento/vista/ventana.php");
-        }
+       }else{
+        return header("Location:http://localhost:8080/requerimiento/vista/Advertencia.php");
+       }
+       
     }
+
+
+    
+
+
 
 
     function observacionProducto($dataproductos)
@@ -68,6 +110,6 @@ date_default_timezone_set('America/Lima');
 
     }
 
-
+/*$codigogenerado=generarCodigo(6,4);*/
 
 ?>
