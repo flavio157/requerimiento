144bf6dd466108b4e061a2ad1e58f907caa82cfe
flavio157 <82402824+flavio157@users.noreply.github.com>
2021-05-15 12:53:25 -0500

    se ternimo la verificacion si recien es registrao en la tabla T_Personal

--- modelo/M_login.php
+++ modelo/M_login.php
@@ -1,6 +1,5 @@
 <?php
 require_once("../db/Usuarios.php");
-require_once("../controlador/C_Funciones.php");
 require_once("../controlador/f_funcion.php");
 class M_Login
 {
@@ -42,13 +41,27 @@ class M_Login
     {
         
        $fechas = fechas($diasprimeraquincena,$diassegundaquincena,$fec_ingreso);
-        
-       /*  $query=$this->db->prepare("SELECT * FROM V_CALL_CENTER  
+     
+        if(!is_string($fechas[0])){
+            $fech1 = $fechas[0]->format('d-m-y');
+            $fech2 = $fechas[1];
+            $bool = $fechas[2];
+            
+        }else{
+            $fech1 = $fechas[0];
+            $fech2 = $fechas[1];
+            $bool = true;
+        }
+
+
+      
+       
+       $query=$this->db->prepare("SELECT * FROM V_CALL_CENTER  
         WHERE VENDEDOR = :cod_vendedor AND FECHA_GENERADO >= :fechaInical 
         AND FECHA_GENERADO < :fechaFinal");
         $query->bindParam("cod_vendedor", $cod_vendedor, PDO::PARAM_STR);
-        $query->bindParam("fechaInical", $fechas[0], PDO::PARAM_STR);
-        $query->bindParam("fechaFinal",  $fechas[1], PDO::PARAM_STR);
+        $query->bindParam("fechaInical", $fech1, PDO::PARAM_STR);
+        $query->bindParam("fechaFinal",  $fech2, PDO::PARAM_STR);
         $query->execute();
         $montoTotal=0;
         
@@ -58,12 +71,11 @@ class M_Login
             }
         }
             
-       if($query){*/
-            return $fechas;
-          /*  return $montoTotal;
+       if($query){
+            return array($montoTotal, $bool);
             $query->closeCursor();
             $query = null;
-        } */
+        } 
     }
 }
 ?>
