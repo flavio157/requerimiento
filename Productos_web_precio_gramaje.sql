5128ae46ae73bd7bcaab0e48512b200847a015ea
flavio157 <82402824+flavio157@users.noreply.github.com>
2021-05-31 17:29:58 -0500

    query de las tablas para los Productos -> Precio web Precio Productos Precios Combo

---
+++ Productos_web_precio_gramaje.sql
@@ -0,0 +1,139 @@
+/*
+00594 EP4 sachet vitaclean     /*producto prueba*/
+00592 EP2 sachet vitaler Pros  /*producto prueba*/
+00591 EP1-SACHET FEMSURE	   /*producto prueba*/
+00593 EP3-SACHET GLUSAN		   /*producto prueba*/	
+*/
+/*Productos que se dieron de baja -> I inactivo*/
+update T_PRODUCTO set EST_PRODUCTO = 'I' where COD_PRODUCTO = '00589'
+update T_PRODUCTO set EST_PRODUCTO = 'I' where COD_PRODUCTO = '00462'
+update T_PRODUCTO set EST_PRODUCTO = 'I' where COD_PRODUCTO = 'A00019'
+update T_PRODUCTO set EST_PRODUCTO = 'I' where COD_PRODUCTO = '00415'
+update T_PRODUCTO set EST_PRODUCTO = 'I' where COD_PRODUCTO = '00280'
+update T_PRODUCTO set EST_PRODUCTO = 'I' where COD_PRODUCTO = '00396'
+update T_PRODUCTO set EST_PRODUCTO = 'I' where COD_PRODUCTO = '00432'
+update T_PRODUCTO set EST_PRODUCTO = 'I' where COD_PRODUCTO = '00434'
+update T_PRODUCTO set EST_PRODUCTO = 'I' where COD_PRODUCTO = '00435'
+update T_PRODUCTO set EST_PRODUCTO = 'I' where COD_PRODUCTO = '00437'
+update T_PRODUCTO set EST_PRODUCTO = 'I' where COD_PRODUCTO = '00590'
+update T_PRODUCTO set EST_PRODUCTO = 'I' where COD_PRODUCTO = '00465'
+update T_PRODUCTO set EST_PRODUCTO = 'I' where COD_PRODUCTO = '00518'
+
+/*Procolagen plus naranja 600g*/
+update T_PRODUCTO set PRECIO_WEB = '130.00',PESO_NETO = '600',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00543' /*PROC*/
+update T_PRODUCTO set PRECIO_WEB = '130.00',PESO_NETO = '600',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00457' /*BP9*/
+
+/*Procolagen Naranja 600g*/
+update T_PRODUCTO set PRECIO_WEB = '120.00',PESO_NETO = '600',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00424' /*AP1*/
+update T_PRODUCTO set PRECIO_WEB = '120.00',PESO_NETO = '600',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00461' /*BL1*/
+
+
+/*Procolagen uva 600g*/
+update T_PRODUCTO set PRECIO_WEB = '120.00',PESO_NETO = '600',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00429' /*AP6*/
+update T_PRODUCTO set PRECIO_WEB = '120.00',PESO_NETO = '600',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00466' /*BL6*/
+
+
+/*Kollagen Naranja 600g*/
+update T_PRODUCTO set PRECIO_WEB = '120.00',PESO_NETO = '600',EST_PRODUCTO = 'A' where COD_PRODUCTO= '00431' /*AP8*/
+
+
+/*Kollagen uva 600g*/
+update T_PRODUCTO set PRECIO_WEB = '120.00',PESO_NETO = '600',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00436' /*BP4*/
+
+
+/*Kollagen naranja 300g*/
+update T_PRODUCTO set PRECIO_WEB = '65.00',PESO_NETO = '300',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00439' /*BP7*/
+
+
+/*Procolagen Naran 300g*/
+update T_PRODUCTO set PRECIO_WEB = '65.00',PESO_NETO = '300',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00438' /*BP6*/
+
+
+/*FLEXOL naranja 600g*/
+update T_PRODUCTO set PRECIO_WEB = '130.00',PESO_NETO = '600',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00580' /*DP3*/
+
+------
+/*Vitaclean 500g*/
+update T_PRODUCTO set PRECIO_WEB = '120.00',PESO_NETO = '500',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00427' /*AP4*/
+update T_PRODUCTO set PRECIO_WEB = '120.00',PESO_NETO = '500',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00464' /*BL4*/
+
+
+/*Vitaler pros 660g*/
+update T_PRODUCTO set PRECIO_WEB = '120.00',PESO_NETO = '660',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00426' /*AP3*/
+update T_PRODUCTO set PRECIO_WEB = '120.00',PESO_NETO = '660',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00433' /*BP1*/
+update T_PRODUCTO set PRECIO_WEB = '120.00',PESO_NETO = '660',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00463' /*BL3*/
+-------
+
+/*MAMGEST 600g*/
+update T_PRODUCTO set PRECIO_WEB = '120.00',PESO_NETO = '600', EST_PRODUCTO = 'A' where COD_PRODUCTO = '00531' /*CP3*/
+ 
+
+/*FEMSURE 600g y 300g*/
+update T_PRODUCTO set PRECIO_WEB = '65.00',PESO_NETO = '300' ,EST_PRODUCTO = 'A' where COD_PRODUCTO = '00588'  /*DP7*/
+update T_PRODUCTO set PRECIO_WEB = '120.00',PESO_NETO = '600',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00532' /*CP4*/
+  
+
+/*METALSHAKE 600 y 300*/
+update T_PRODUCTO set PRECIO_WEB = '65.00',PESO_NETO = '300' ,EST_PRODUCTO = 'A' where COD_PRODUCTO = '00587' /*DP6*/
+update T_PRODUCTO set PRECIO_WEB = '120.00',PESO_NETO = '600',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00533' /*CP5*/
+ 
+
+/*DISLIGRAS 600*/
+update T_PRODUCTO set PRECIO_WEB = '130.00',PESO_NETO = '600', EST_PRODUCTO = 'A' where COD_PRODUCTO = '00582' /*DP5*/
+
+
+/*GLUZAN 1k*/
+update T_PRODUCTO set PRECIO_WEB = '150.00',PESO_NETO = '1000',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00581' /*DP4*/
+ 
+ 
+/*ENFAFORT 1k*/   
+update T_PRODUCTO set PRECIO_WEB = '130.00',PESO_NETO = '1000',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00578' /*DP1*/
+update T_PRODUCTO set PRECIO_WEB = '130.00',PESO_NETO = '1000',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00428' /*AP5*/
+ 
+/*PLUS FORT 1k*/ 
+update T_PRODUCTO set PRECIO_WEB = '130.00',PESO_NETO = '1000',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00579' /*DP2*/
+
+/*MORE ENERGY*/ 
+update T_PRODUCTO set PRECIO_WEB = '140.00',PESO_NETO = '1000',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00419' /*AJ5*/
+update T_PRODUCTO set PRECIO_WEB = '140.00',PESO_NETO = '1000',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00430' /*AP7*/
+    
+/*ISO MORE ENERGY */ 
+update T_PRODUCTO set PRECIO_WEB = '140.00',PESO_NETO = '1000' , EST_PRODUCTO = 'A' where COD_PRODUCTO = '00459' /*BP8*/
+
+/*SACHET PROCOL.PLUS sachet 10g*/
+update T_PRODUCTO set PRECIO_WEB = '110.00',PESO_NETO = '1000' ,EST_PRODUCTO = 'A' where COD_PRODUCTO = '00541' /*SA5*/
+ 
+ 
+/*00472 CP2-Kollagen uva 300*/
+update T_PRODUCTO set PRECIO_WEB = '65.00',PESO_NETO = '300',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00472' /*CP2*/ 
+ 
+/*00471 CP1 Procolagen uva 300*/
+update T_PRODUCTO set PRECIO_WEB = '65.00',PESO_NETO = '300',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00471' /*CP1*/
+
+/*00594 EP4 sachet vitaclean  producto Prueba*/
+update T_PRODUCTO set PRECIO_WEB = '',PESO_NETO = '',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00594' /*EP4*/ 
+
+/*00592 EP2 sachet vitaler Prosn producto Prueba*/
+update T_PRODUCTO set PRECIO_WEB = '',PESO_NETO = '',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00594' /*EP2*/ 
+ 
+/*00591 EP1-SACHET FEMSURE Producto Prueba*/
+update T_PRODUCTO set PRECIO_WEB = '',PESO_NETO = '',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00594' /*EP1*/  
+
+/*00593 EP3-SACHET GLUSAN Producto Prueba*/
+update T_PRODUCTO set PRECIO_WEB = '',PESO_NETO = '',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00593' /*EP3*/   
+
+/*00425 AP2-PLUSFORT X750 */
+update T_PRODUCTO set PRECIO_WEB = '120.00',PESO_NETO = '750',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00425' /*AP2*/ 
+
+/*00455	MAGNESIO 140 GRS */
+update T_PRODUCTO set PRECIO_WEB = '140.00',PESO_NETO = '140' ,EST_PRODUCTO = 'A' where COD_PRODUCTO = '00455' 
+
+/*00534	CP6 VITA FE (ANEMIA) X 300*/
+update T_PRODUCTO set PRECIO_WEB = '65.00',PESO_NETO = '300',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00534' 
+
+
+/*00590	DP9-SACHET DESLIGRAS Producto Prueba*/
+update T_PRODUCTO set PRECIO_WEB = '',PESO_NETO = '' ,EST_PRODUCTO = 'A' where COD_PRODUCTO = '00590' /*DP9*/ 
+
+/*00529	CP8 COLAG.PLUS NAR X300*/
+update T_PRODUCTO set PRECIO_WEB = '70',PESO_NETO = '300',EST_PRODUCTO = 'A' where COD_PRODUCTO = '00529' /*CP8*/ 
